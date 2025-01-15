9cdc56a6cfa8acefa9b7e9665667970db6d2d881
İbrahim Yılgör <yilgor16@itu.edu.tr>
2025-01-15 19:42:27 +0300

    chore: unused divider

--- src/Table.jsx
+++ src/Table.jsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Chip, Divider } from "@mui/material";
+import { Chip } from "@mui/material";
 
 export default function BasicTable({ data }) {
   return (
